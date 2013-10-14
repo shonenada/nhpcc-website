573e3678f842a0b70bdc3ce315b408604bf8c0a4
shonenada <shonenada@gmail.com>
2013-10-14 21:07:56 +0800

    Added index.php

---
+++ wwwroot/index.php
@@ -0,0 +1,17 @@
+<?php
+
+define("DOCROOT", realpath(__DIR__ . "/../") . DIRECTORY_SEPARATOR);
+define("PKGROOT", realpath(DOCROOT. "vendor/"). DIRECTORY_SEPARATOR);
+define("APPROOT", realpath(DOCROOT. "nhpcc/"). DIRECTORY_SEPARATOR);
+define("WEBROOR", realpath(DOCROOT. "wwwroot/"). DIRECTORY_SEPARATOR);
+
+require_once(PKGROOT. "autoload.php");
+require_once(APPROOT. 'app.php');
+
+$app = create_app();
+
+$app->get('/', function() use ($app){
+    $app->render("index.html");
+});
+
+$app->run();
