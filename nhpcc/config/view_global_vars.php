f6ffcb0d5882bcc9752580490515d18e696cbeb7
shonenada <shonenada@gmail.com>
2013-10-15 01:23:11 +0800

    add html files.

--- nhpcc/config/view_global_vars.php
+++ nhpcc/config/view_global_vars.php
@@ -3,5 +3,7 @@
 // Define global variables for view
 
 return array(
-    'webtitle' => '国家高性能计算中心深圳分中心',
+    'webTitle' => '国家高性能计算中心深圳分中心',
+    'webKeywords' => '深圳大学,计算机与软件学院,高性能计算中心',
+    'webDescription' => '国家高性能计算中心深圳分中心'
 );
