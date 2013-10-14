15ba29002caf46a75d06f5ad69dd627c575a8742
shonenada <shonenada@gmail.com>
2013-10-15 01:21:12 +0800

    Update copy script.

--- copy.py
+++ copy.py
@@ -1,15 +1,24 @@
 import os
-from shutil import copytree
+from shutil import copytree, rmtree
 
 
 ROOT = os.path.dirname(__file__)
 APP_ROOT = os.path.join(ROOT, "nhpcc")
 WEB_ROOT = os.path.join(ROOT, 'wwwroot')
 
-src_path = os.path.join(APP_ROOT, 'assets', 'components')
-dst_path = os.path.join(WEB_ROOT, 'static', 'components')
 
-copytree(src_path, dst_path)
+dir_join = lambda path: (os.path.join(APP_ROOT, 'assets', path),
+                         os.path.join(WEB_ROOT, 'static', path))
 
+def copy_dirs(src_path, dst_path):
+    '''delete files and folders'''
+    if os.path.isdir(dst_path):
+        rmtree(dst_path)
+        print 'Deleting', dst_path
+    copytree(src_path, dst_path)
+
+
+paths = [dir_join('components'), dir_join('styles'), dir_join('scripts')]
+[copy_dirs(src_path, dst_path) for src_path, dst_path in paths]
 
 print "Finished copying"
