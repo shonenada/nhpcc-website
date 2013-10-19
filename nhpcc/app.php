606917fa9720e3e4122c68e8e4784b03caa9d3dc
shonenada <shonenada@gmail.com>
2013-10-19 18:57:04 +0800

    Split config.

--- nhpcc/app.php
+++ nhpcc/app.php
@@ -4,8 +4,9 @@ require_once(APPROOT. "extensions.php");
 require_once(APPROOT. "controller.php");
 
 // Create app using factory.
-function create_app($config_filename="config.php"){
-    $config = require_once(APPROOT. "config/". $config_filename);
+function create_app($config_filename="custom.php"){
+    $config = require_once(APPROOT. "config/config.php");
+    $custom = require_once(APPROOT. "config/". $config_filename);
     $app = new \Slim\Slim($config);
 
     setup_views($app);
