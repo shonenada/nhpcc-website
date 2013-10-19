99cf1ea823c52c4ab756e50071d76dfcc9d7b516
shonenada <shonenada@gmail.com>
2013-10-19 15:08:24 +0800

    Added sub page

--- nhpcc/config/twig/categories.php
+++ nhpcc/config/twig/categories.php
@@ -1,39 +1,39 @@
 <?php
 
 return array(
-    array("title" => "首页", "link" => "/", "id" => "home"),
-    array("title" => "实验室概况", "link" => "/overview", "sub" => array(
-            array("title" => "实验室简介", "link" => "/overview/introduction"),
-            array("title" => "学术委员会", "link" => "/overview/academic"),
-            array("title" => "研究队伍", "link" => "/overview/team")
+    "home" => array("title" => "首页", "url" => "/", "id" => "home"),
+    "overview" => array("title" => "实验室概况", "url" => "/overview", "sub" => array(
+            array("title" => "实验室简介", "url" => "/overview/introduction"),
+            array("title" => "学术委员会", "url" => "/overview/academic"),
+            array("title" => "研究队伍", "url" => "/overview/team")
         )),
-    array("title" => "研究方向", "link" => "/research"),
-    array("title" => "承担项目", "link" => "/projects", "sub" => array(
-            array("title" => "纵向课题", "link" => "/projects/l"),
-            array("title" => "横向课题", "link" => "/projects/t")
+    "research" => array("title" => "研究方向", "url" => "/research"),
+    "projects" => array("title" => "承担项目", "url" => "/projects", "sub" => array(
+            array("title" => "纵向课题", "url" => "/projects/l"),
+            array("title" => "横向课题", "url" => "/projects/t")
         )),
-    array("title" => "科研成果", "link" => "/achievements", "sub" => array(
-            array("title" => "论文", "link" => "/achievements/theies"),
-            array("title" => "专著", "link" => "/achievements/monographs"),
-            array("title" => "获奖", "link" => "/achievements/awards"),
-            array("title" => "专利", "link" => "/achievements/patents")
+    "achievements" => array("title" => "科研成果", "url" => "/achievements", "sub" => array(
+            array("title" => "论文", "url" => "/achievements/theies"),
+            array("title" => "专著", "url" => "/achievements/monographs"),
+            array("title" => "获奖", "url" => "/achievements/awards"),
+            array("title" => "专利", "url" => "/achievements/patents")
         )),
-    array("title" => "学术交流", "link" => "/exchange", "sub" => array(
-            array("title" => "学术活动", "link" => "/exchange/activities"),
-            array("title" => "学术报告", "link" => "/exchange/reports")
+    "exchange" => array("title" => "学术交流", "url" => "/exchange", "sub" => array(
+            array("title" => "学术活动", "url" => "/exchange/activities"),
+            array("title" => "学术报告", "url" => "/exchange/reports")
         )),
-    array("title" => "开放基金", "link" => "/foundation", "sub" => array(
-            array("title" => "开放课题管理办法", "link" => "/foundation/manage"),
-            array("title" => "开放课题申请指南", "link" => "/foundation/guide"),
-            array("title" => "开放课题目录", "link" => "/foundation/list"),
-            array("title" => "开放课题合同书", "link" => "/foundation/contract"),
+    "foundation" => array("title" => "开放基金", "url" => "/foundation", "sub" => array(
+            array("title" => "开放课题管理办法", "url" => "/foundation/manage"),
+            array("title" => "开放课题申请指南", "url" => "/foundation/guide"),
+            array("title" => "开放课题目录", "url" => "/foundation/list"),
+            array("title" => "开放课题合同书", "url" => "/foundation/contract"),
         )),
-    array("title" => "人才培养", "link" => "/train", "sub" => array(
-            array("title" => "导师名单", "link" => "/train/teachers"),
-            array("title" => "研究生名单", "link" => "/train/graduate"),
-            array("title" => "导师名单", "link" => "/train/loongson"),
+    "train" => array("title" => "人才培养", "url" => "/train", "sub" => array(
+            array("title" => "导师名单", "url" => "/train/teachers"),
+            array("title" => "研究生名单", "url" => "/train/graduate"),
+            array("title" => "导师名单", "url" => "/train/loongson"),
         )),
-    array("title" => "规章制度", "link" => "/rules"),
-    array("title" => "广纳贤才", "link" => "/finding"),
-    array("title" => "下载中心", "link" => "/download"),
+    "rules" => array("title" => "规章制度", "url" => "/rules"),
+    "finding" => array("title" => "广纳贤才", "url" => "/finding"),
+    "download" => array("title" => "下载中心", "url" => "/download"),
 );
