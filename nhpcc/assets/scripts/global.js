0d5d0171e2abb83ea4ee84e8bff3afb7124295d2
shonenada <shonenada@gmail.com>
2013-11-01 04:45:40 +0800

    update.

--- nhpcc/assets/scripts/global.js
+++ nhpcc/assets/scripts/global.js
@@ -1,46 +1,47 @@
-(function(window, $) {	
-	var nhpcc = {};
+(function(window, $) {    
+    var nhpcc = {};
 
-	// 初始化全站统一元素
-	nhpcc.elementInit = function() {
-		$('body').prepend($('<div id="message-box"></div>')); // 信息框容器
-	};
+    // 初始化全站统一元素
+    nhpcc.elementInit = function() {
+        $('body').prepend($('<div id="message-box"></div>')); // 信息框容器
+    };
 
-	// 全站统一信息提示框
-	nhpcc._messagebox = function(message, timeout, specialClass) {
-		var entity = $('<div class="message-content"></div>')
-			.hide()
-			.append($('<span class="message-text"></span>').append(message))
-			.append($('<a href="javascript:void(0);" class="button message-close-btn">关闭</a>'))
-			.addClass(specialClass);
+    // 全站统一信息提示框
+    nhpcc._messagebox = function(message, timeout, specialClass) {
+        var entity = $('<div class="message-content"></div>')
+            .hide()
+            .append($('<span class="message-text"></span>').append(message))
+            .append($('<a href="javascript:void(0);" class="button message-close-btn">关闭</a>'))
+            .addClass(specialClass);
 
-		$('#message-box').append(entity);
+        $('#message-box').append(entity);
 
-		entity.fadeIn(500);
+        entity.fadeIn(500);
 
-		if (timeout > 0) {
-			setTimeout(function(){
-				entity.fadeOut(500, function(){
-					$(this).remove();
-				});
-			}, timeout);
-		}
+        if (timeout > 0) {
+            setTimeout(function(){
+                entity.fadeOut(500, function(){
+                    $(this).remove();
+                });
+            }, timeout);
+        }
 
-		entity.children('a.message-close-btn').click(function(){
-			entity.fadeOut(500, function(){
-				$(this).remove();
-			});
-		});
-	};
+        entity.children('a.message-close-btn').click(function(){
+            entity.fadeOut(500, function(){
+                $(this).remove();
+            });
+        });
+    };
 
-	nhpcc.notice = function(message, timeout) { this._messagebox(message, timeout, 'notice'); };
-	nhpcc.alert  = function(message, timeout) { this._messagebox(message, timeout, 'alert');  };
-	nhpcc.error  = function(message, timeout) { this._messagebox(message, timeout, 'error'); };
-	
-	window.nhpcc = window.$S = nhpcc;
+    nhpcc.notice = function(message, timeout) { this._messagebox(message, timeout, 'notice'); };
+    nhpcc.alert  = function(message, timeout) { this._messagebox(message, timeout, 'alert');  };
+    nhpcc.error  = function(message, timeout) { this._messagebox(message, timeout, 'error'); };
+    nhpcc.redirect = function(path, timeout) { setTimeout(function(){location.href = path;}, timeout); };
+    
+    window.nhpcc = window.$S = nhpcc;
 })(window, jQuery);
 
 $(function(){
-	// 初始化元素
-	$S.elementInit();
+    // 初始化元素
+    $S.elementInit();
 });
