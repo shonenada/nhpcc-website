27f73775e1d4d686492b8a0144cf2ddf09d1663d
shonenada <shonenada@gmail.com>
2013-11-01 01:43:27 +0800

    Longer cookie lifetime.

--- nhpcc/config/config.php
+++ nhpcc/config/config.php
@@ -10,7 +10,7 @@ return array_merge(
         'view' => new \Slim\Views\Twig(),
         'templates.path' => APPROOT. '/templates',
         'cookies.secure' => true,
-        'cookies.lifetime' => '20 minutes',
+        'cookies.lifetime' => '30 days',
         'cookies.cipher' => MCRYPT_RIJNDAEL_256,
         'cookies.cipher_mode' => MCRYPT_MODE_CBC,
         'http.version' => '1.1',
