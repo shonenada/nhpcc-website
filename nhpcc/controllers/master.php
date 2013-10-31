29f4c995be9c04ac633b1b9e702c20988bd360c9
shonenada <shonenada@gmail.com>
2013-11-01 01:44:42 +0800

    update feature.

--- nhpcc/controllers/master.php
+++ nhpcc/controllers/master.php
@@ -16,5 +16,29 @@ return array(
             $app->render("admin/status.html");
         });
 
+        $app->get("/admin/account", function() use($app) {
+            $page = $app->request->params("page") ? $app->request->params("page") : 1;
+            $users = User::getList($page);
+            $app->render("admin/account.html", get_defined_vars());
+        });
+
+        $app->get("/admin/account/:id", function($id) use($app) {
+            $action = "view";
+            $queryUser = User::find($id);
+            $app->render("admin/account-view.html", get_defined_vars());
+        });
+
+        $app->delete("/admin/account/:id", function($id) use($app) {
+            $queryUser = User::find($id);
+            $queryUser->setLevel(-1);
+            $output = array("success" => true,
+                            "messaeg" => array("删除成功"));
+        });
+
+        $app->get("/admin/account/:id/edit", function($id) use($app) {
+            $action = "edit";
+            $queryUser = User::find($id);
+            $app->render("admin/account-view.html", get_defined_vars());
+        });
     }
 );
