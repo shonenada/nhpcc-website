713e048592287bc865b4fa88cca94bdd61ff0061
shonenada <shonenada@gmail.com>
2013-11-01 03:16:12 +0800

    Update user controllers/

--- nhpcc/controllers/user.php
+++ nhpcc/controllers/user.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once(APPROOT . "models/user.php");
 require_once(APPROOT . "utils.php");
 
 return array(
@@ -14,6 +13,14 @@ return array(
             $app->render("login.html");
         });
 
+        $app->post("/logout", function() use ($app) {
+            $app->deleteCookie("user_id");
+            $app->deleteCookie("token");
+            $output = array("success" => true,
+                            "message" => array("退出成功"));
+            echo json_encode($output);
+        });
+
         $app->post("/login", function() use ($app) {
 
             $salt = $app->config("salt");
@@ -22,27 +29,26 @@ return array(
 
             if (!isset($username) || strlen($username) < 1){
                 $output = array("success" => false,
-                                "message" => "用户名不能为空");
+                                "message" => array("用户名不能为空"));
                 echo json_encode($output);
                 return ;
             }
 
             if (!isset($password) || strlen($password) < 1){
                 $output = array("success" => false,
-                                "message" => "密码不能为空");
+                                "message" => array("密码不能为空"));
                 echo json_encode($output);
                 return ;
             }else{
                 $password = User::hashPassword($password, $salt);
             }
 
-            $queryUser = $app->environment['em']->getRepository("User")
-                                                ->findOneBy(array(
-                                                     "username" => $username,
-                                                     "password" => $password));
+            $queryUser = User::findOneBy(array("username" => $username,
+                                               "password" => $password));
+
             if ($queryUser == NULL){
                 $output = array("success" => false,
-                                "message" => "帐号或密码错误！");
+                                "message" => array("帐号或密码错误！"));
                 echo json_encode($output);
                 return ;
             }
@@ -52,13 +58,13 @@ return array(
                 $now = new DateTime("now");
                 $queryUser->setIP($ip);
                 $queryUser->setLastLogin($now);
-                $app->environment['em']->persist($queryUser);
-                $app->environment['em']->flush();
+                $queryUser->save();
+                $queryUser->flush();
                 $token = generateToken($ip, $now, $salt);
                 $app->setEncryptedCookie("user_id", $uid);
                 $app->setEncryptedCookie("token", $token);
                 $output = array("success" => true,
-                                "message" => "登录成功");
+                                "message" => array("登录成功"));
                 echo json_encode($output);
                 return ;
             }
