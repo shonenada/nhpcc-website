fae05cb6926b0d3990ca21c64cee26e7f4eb059d
shonenada <shonenada@gmail.com>
2013-12-06 00:49:37 +0800

    add project controller and template.

--- nhpcc/controllers/projects.php
+++ nhpcc/controllers/projects.php
@@ -1,4 +1,6 @@
 <?php
+require_once(APPROOT . "models/User.php");
+require_once(APPROOT . "models/Article.php");
 
 return array(
     "export" => function($app) {
@@ -8,6 +10,9 @@ return array(
         $app->get("/projects", function() use($app, $cat) {
             $nav = $cat['projects'];
             $categories = $nav['sub'];
+            $projects_l = Article::getSpecList(Article::getCat("PROJECT_L"), 1 ,10);
+            $projects_t = Article::getSpecList(Article::getCat("PROJECT_T"), 1 ,10);
+            $projects = array_unique(array_merge($projects_l, $projects_t));
             $app->render("projects/index.html", get_defined_vars());
         });
 
@@ -15,6 +20,7 @@ return array(
             $nav = $cat['projects'];
             $categories = $nav['sub'];
             $sub = $categories['l'];
+            $projects = Article::getSpecList(Article::getCat("PROJECT_L"), 1 ,10);
             $app->render("projects/projects_l.html", get_defined_vars());
         });
 
@@ -22,6 +28,7 @@ return array(
             $nav = $cat['projects'];
             $categories = $nav['sub'];
             $sub = $categories['t'];
+            $projects = Article::getSpecList(Article::getCat("PROJECT_T"), 1 ,10);
             $app->render("projects/projects_l.html", get_defined_vars());
         });
     }
