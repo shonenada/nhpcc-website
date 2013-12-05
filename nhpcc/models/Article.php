7f428eaf28a1ab3e27b4cc3199ad4ddcb1524936
shonenada <shonenada@gmail.com>
2013-12-06 00:39:59 +0800

    Update article model.

--- nhpcc/models/Article.php
+++ nhpcc/models/Article.php
@@ -10,15 +10,51 @@
  * @property string      $content
  * @property datetime    $created
  * @property integer     $readCount
+ * @property integer     $category
  * @property integer     $deleted
  **/
 
 class Article extends ModelBase {
 
-    const NEWS = 1;
-    const ANNOUNCE = 5;
-    const ACTIVATE = 9;
-    
+    static public $categories = array(
+        'INDEX' => array('首页', array(
+            'NEWS' => array(1, '新闻简报'),
+            'ANNOUNCE' => array(5, '通知公告'),
+            'ACTIVATY' => array(9, '学术活动'),
+            )),
+        'PROJECT' => array('承担项目', array(
+            'L' => array(11, '纵向课题'),
+            'T' => array(12, '横向课题'),
+            )),
+        'ACHIE' => array('科研成果', array(
+            'THESIS' => array(21, '论文'),
+            'MONOGRAPH' => array(22, '专著'),
+            'AWARD' => array(23, '获奖'),
+            'PATENT' => array(24, '专利'),
+            )),
+        'EXCHANGE' => array('学术交流', array(
+            'ACTIVATY' => array(31, '学术活动'),
+            'REPORT' => array(32, '学术报告'),
+            )),
+        'FOUNDATION' => array('开放基金', array(
+            'MANAEG' => array(41, '开放课题管理办法'),
+            'GUIDE' => array(42, '开放课题申请指南'),
+            'LIST' => array(43, '开放课题目录'),
+            'CONTRACT' => array(44, '开放课题合同书'),
+            )),
+        'TRAIN' => array('人才培养', array(
+            'TEACHERS' => array(51, '导师'),
+            'GRADUATE' => array(52, '研究生'),
+            'LOONGSON' => array(53, '龙芯奖学金'),
+            )),
+        'RULES' => array('规章制度', array(
+            'RULES' => array(61, '规章制度 '),
+            )),
+        'FINDING' => array('招纳贤才', array(
+            'FINDING' => array(71, '招纳贤才'),
+            )),
+        );
+
     /**
      * @Column(name="id", type="integer")
      * @Id
@@ -128,6 +164,11 @@ class Article extends ModelBase {
         $this->category = 0;
         $this->readCount = 0;
         $this->deleted = 0;
+    }
+
+    static public function getCat ($input) {
+        $arr = explode('_', $input);
+        return Article::$categories[$arr[0]][1][$arr[1]][0];
     }
 
     static public function getList($page=1, $pagesize=20, $asc=false) {
