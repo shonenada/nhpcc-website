653ad24a61789738e7f02cfe5834bb5eaa8101a9
shonenada <shonenada@gmail.com>
2014-02-21 15:49:52 +0800

    improve controllers loader

--- nhpcc/controller.php
+++ nhpcc/controller.php
@@ -2,29 +2,21 @@
 
 function init_controllers($app){
 
-    $master_home = require(APPROOT. "controllers/master/home.php");
-    $master_slider = require(APPROOT. "controllers/master/admin_slider.php");
-    $master_account = require(APPROOT. "controllers/master/admin_account.php");
-    $master_article = require(APPROOT. "controllers/master/admin_article.php");
-    $article = require(APPROOT. "controllers/articles.php");
-    $overview = require(APPROOT. "controllers/overview.php");
-    $user = require(APPROOT. "controllers/user.php");
-    $project = require(APPROOT. "controllers/projects.php");
-    $research = require(APPROOT. "controllers/research.php");
-    $achievements = require(APPROOT. "controllers/achievements.php");
-    $exchage = require(APPROOT. "controllers/exchage.php");
-    $foundation = require(APPROOT. "controllers/foundation.php");
-    $train = require(APPROOT. "controllers/train.php");
-    $rules = require(APPROOT. "controllers/rules.php");
-    $finding = require(APPROOT. "controllers/finding.php");
-    $download = require(APPROOT. "controllers/download.php");
-
-    $controllers = array($master_home, $master_slider, $master_account,
-                         $master_article, $overview, $user, $article, $project,
-                         $research, $achievements, $exchage, $foundation,
-                         $train, $rules, $finding, $download);
-    foreach($controllers as $c){
-        $func = $c['export'];
-        $func($app);
+    function scan_controller($path, $app){
+        foreach (glob($path) as $filename){
+            if (is_dir($filename)){
+                scan_controller($filename . "/*", $app);
+            } else {
+                if (substr($filename, -3) == 'php'){
+                    $export = require($filename);
+                    $func = $export['export'];
+                    $func($app);
+                }
+            }
+        }
     }
+
+    $scan_path = APPROOT . DIRECTORY_SEPARATOR . "controllers/*";
+    scan_controller($scan_path, $app);
+
 }
