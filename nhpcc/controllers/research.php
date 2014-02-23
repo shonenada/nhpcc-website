15f5468e9f4e7e2a71c70d52707ddb1259d9a5fe
shonenada <shonenada@gmail.com>
2014-02-23 20:12:01 +0800

    update

--- nhpcc/controllers/research.php
+++ nhpcc/controllers/research.php
@@ -5,32 +5,26 @@ use \Utils;
 return array(
     "export" => function($app) {
 
-        $cat = Utils::loadStaticContent('categories')->getContent();
+        $cat = Utils::loadStaticContent('categories', true)->getContent();
+        $research = Utils::loadStaticContent('research', true)->getContent();
 
-        $categories = array(
-            "system" => array('title' => '普及型高性能计算机体系结构', 'url' => "/research/system"),
-            "theory" => array('title' => '普及型高性能计算机系统软件及理论', 'url' => "/research/theory"),
-            "security" => array('title' => '普及型高性能计算安全防护', 'url' => "/research/security"),
-            "sustain" => array('title' => '普及型高性能计算机应用基础理论和支撑技术', 'url' => "/research/sustain"),
-        );
-
-        $app->get("/research", function() use($app, $cat) {
+        $app->get("/research", function() use ($app, $cat) {
             return $app->redirect("/research/system");
         });
 
-        $app->get("/research/system", function() use($app, $cat, $categories) {
+        $app->get("/research/system", function() use ($app, $cat, $research) {
             $nav = $cat['research'];
             return $app->render("research/system.html", get_defined_vars());
         });
-        $app->get("/research/theory", function() use($app, $cat, $categories) {
+        $app->get("/research/theory", function() use ($app, $cat, $research) {
             $nav = $cat['research'];
             return $app->render("research/theory.html", get_defined_vars());
         });
-        $app->get("/research/security", function() use($app, $cat, $categories) {
+        $app->get("/research/security", function() use ($app, $cat, $research) {
             $nav = $cat['research'];
             return $app->render("research/security.html", get_defined_vars());
         });
-        $app->get("/research/sustain", function() use($app, $cat, $categories) {
+        $app->get("/research/sustain", function() use ($app, $cat, $research) {
             $nav = $cat['research'];
             return $app->render("research/sustain.html", get_defined_vars());
         });
