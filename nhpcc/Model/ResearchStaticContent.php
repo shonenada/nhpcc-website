7b409a1db866b75ee0eb0e0dc1740e7150ac1ff0
shonenada <shonenada@gmail.com>
2014-02-23 21:50:04 +0800

    improve model of ResearchStaticContent

--- nhpcc/Model/ResearchStaticContent.php
+++ nhpcc/Model/ResearchStaticContent.php
@@ -2,27 +2,16 @@
 
 namespace Model;
 
-function merge ($arr, $idx) {
-    return array_merge($arr[$idx], array('id' => $idx, 'label' => $arr[$idx]['title']));
-}
-
-function modify ($arr, $idx, $params) {
-    if (isset($params[$idx.'_title']))
-        $arr[$idx]['title'] = $params[$idx.'_title'];
-    if (isset($params[$idx.'_content']))
-        $arr[$idx]['content'] = $params[$idx.'_content'];
-    return $arr;
-}
 
 class ResearchStaticContent extends StaticContent {
 
     public function render () {
         $this->toArray();
         $content = $this->getContent();
-        $content['system'] = merge($content, 'system');
-        $content['theory'] = merge($content, 'theory');
-        $content['security'] = merge($content, 'security');
-        $content['sustain'] = merge($content, 'sustain');
+
+        foreach (array_keys($content) as $idx)
+            $content[$idx] = $this->merge($content, $idx);
+
         return $content;
     }
 
@@ -30,13 +19,23 @@ class ResearchStaticContent extends StaticContent {
         $this->toArray();
         $content = $this->getContent();
 
-        $content = modify($content, 'system', $params);
-        $content = modify($content, 'theory', $params);
-        $content = modify($content, 'security', $params);
-        $content = modify($content, 'sustain', $params);
+        foreach (array_keys($content) as $idx)
+            $content = $this->modify($content, $idx, $params);
 
         $this->setContent($content);
         $this->save();
+    }
+
+    private function merge ($arr, $idx) {
+        return array_merge($arr[$idx], array('id' => $idx, 'label' => $arr[$idx]['title']));
+    }
+
+    private function modify ($arr, $idx, $params) {
+        if (isset($params[$idx.'_title']))
+            $arr[$idx]['title'] = $params[$idx.'_title'];
+        if (isset($params[$idx.'_content']))
+            $arr[$idx]['content'] = $params[$idx.'_content'];
+        return $arr;
     }
 
 }
