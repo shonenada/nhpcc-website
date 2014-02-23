aceda732fd790e56709d8e2624357f53f5f6d260
shonenada <shonenada@gmail.com>
2014-02-24 00:17:41 +0800

    clean achievenments.

--- nhpcc/controllers/achievements.php
+++ nhpcc/controllers/achievements.php
@@ -1,53 +1,21 @@
 <?php
 
 use \Utils;
-use \Model\Article;
 
 return array(
-    "export" => function($app) {
+    'export' => function($app) {
+        $cat = Utils::loadStaticContent('category', true)->getContent();
+        $achievements = Utils::loadStaticContent('achievements', true)->getContent();
 
-        $cat = Utils::loadStaticContent('category')->getContent();
-        $achievements = require(APPROOT. 'static_contents/achievements.php');
-
-        $app->get("/achievements", function() use($app, $cat) {
-            return $app->redirect("/achievements/theies");
+        $app->get('/achievements', function() use ($app, $cat) {
+            return $app->redirect('/achievements/theies');
         });
 
-        $app->get("/achievements/theies", function() use($app, $cat) {
+        $app->get('/achievements/:type', function ($type) use ($app, $cat, $achievements) {
             $nav = $cat['achievements'];
             $categories = $nav['sub'];
-            $sub = $categories['theies'];
-            return $app->render("achievements/theies.html", get_defined_vars());
-        });
-
-        $app->get("/achievements/monographs", function() use($app, $cat) {
-            $nav = $cat['achievements'];
-            $categories = $nav['sub'];
-            $sub = $categories['monographs'];
-            return $app->render("achievements/monographs.html", get_defined_vars());
-        });
-
-        $app->get("/achievements/awards", function() use($app, $cat, $achievements) {
-            $nav = $cat['achievements'];
-            $categories = $nav['sub'];
-            $sub = $categories['awards'];
-            $awards = $achievements['awards'];
-            return $app->render("achievements/awards.html", get_defined_vars());
-        });
-
-        $app->get("/achievements/patents", function() use($app, $cat) {
-            $nav = $cat['achievements'];
-            $categories = $nav['sub'];
-            $sub = $categories['patents'];
-            return $app->render("achievements/patents.html", get_defined_vars());
-        });
-
-        $app->get("/achievements/:id", function($id) use($app, $cat) {
-            $nav = $cat['achievements'];
-            $categories = $nav['sub'];
-            $article = Article::find($id);
-            return $app->render("sub.html", get_defined_vars());
-        })->conditions(array("id" => "\d+"));
-
+            $sub = $categories[$type];
+            return $app->render("achievements/{$type}.html", get_defined_vars());
+        })->conditions(array('type' => 'theies|monographs|awards|patents'));
     }
 );
