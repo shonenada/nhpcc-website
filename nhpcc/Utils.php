7b20da68babbd9b31646855f70fbee3d3fac8550
shonenada <shonenada@gmail.com>
2014-02-23 16:11:15 +0800

    rename utils.php => Utils.php

---
+++ nhpcc/Utils.php
@@ -0,0 +1,21 @@
+<?php
+
+use \Model\StaticContent;
+
+class Utils {
+
+    static public function generateToken($ip, $now, $salt){
+        $now = $now->format('Y-m-d H:i:s');
+        return md5 ("{$now}{{$salt}}{$ip}");
+    }
+
+    static public function require_controller ($controller_name) {
+        return require_once (APPROOT . "controllers/{$controller_name}.php");
+    }
+
+    static public function loadStaticContent ($module) {
+        $return = StaticContent::loadFromFile(APPROOT . 'static_contents/' . str_replace('.', '/', $module) . '.json');
+        return $return;
+    }
+
+}
