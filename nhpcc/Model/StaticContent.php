0d7587f2664022854d28b087311c308f71581c3f
shonenada <shonenada@gmail.com>
2014-02-23 15:54:44 +0800

    add StaticContent Model

---
+++ nhpcc/Model/StaticContent.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Model;
+
+// A class to handle static content which stored in json file.
+// Not a doctrine Model.
+
+class StaticContent {
+
+    const FULL_TEMPLATE = 'full_static_template';
+    const DOUBLE_COLUMN_TEMPLATE = 'double_column_static_template';
+
+    private $title;
+    private $content;
+    private $template;
+
+    public function getTitle () {
+        return $this->title;
+    }
+
+    public function setTitle ($title) {
+        $this->title = $title;
+    }
+
+    public function getContent () {
+        return $this->content;
+    }
+
+    public function setContent ($content) {
+        $this->content = $content;
+    }
+
+    public function setTemplate ($template) {
+        $this->template = $template;
+    }
+
+    public function getTemplate () {
+        return $this->template;
+    }
+
+    public function saveTo ($filepath) {
+        $contentToSave = array(
+            'title' => $this->title,
+            'template' => $this->template,
+            'content' => $this->content,
+        );
+        $jsonContent = json_encode($contentToSave);
+        file_put_contents($filepath, $jsonContent);
+    }
+
+    static public function loadFromFile ($filepath) {
+        $fileContent = file_get_contents($filepath);
+        $jsonContent = json_decode($fileContent);
+        $return = new StaticContent();
+        $return->setTitle($jsonContent->title);
+        $return->setContent($jsonContent->content);
+        $return->setTemplate($jsonContent->template);
+        return $return;
+    }
+
+}
